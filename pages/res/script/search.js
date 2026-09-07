1de5246f2087b33ceb7a7787ddb28eba22b02f35
w-dev255 <devangleteam@gmail.com>
2026-09-07 17:33:48 +0200

    update

---
+++ pages/res/script/search.js
@@ -0,0 +1,22 @@
+const path = {
+	"0": "pages/C.html",
+	"1": "pages/tools.html",
+	"2": "pages/music.html"
+};
+
+let input = document.getElementById("search");
+
+input.addEventListener("input", () => {
+	let buffer = input.value
+
+	if (buffer === "C") {
+		window.location.href = path["0"];
+	}
+	if (buffer === "Tools") {
+		window.location.href = path["1"];
+	}
+	if (buffer == "Music") {
+		window.location.href = path["2"];
+	}
+});
+
