55eecfe66ad21554c1920f81584971c600a77f0b
w-dev255 <devangleteam@gmail.com>
2026-09-08 20:46:51 +0200

    update

---
+++ test/C/do_while_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+ int edge = 10;
+
+ int max_edge = 0;
+
+ do {
+
+  printf("%d\n", edge);
+
+ } while (edge < max_edge);
+
+ return 0;
+
+}
