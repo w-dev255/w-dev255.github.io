8b9059541ea40ebd8c491dfa89ec77a6ff0b2ec2
w-dev255 <devangleteam@gmail.com>
2026-09-10 11:31:56 +0200

    update

--- res/script/quiz.js
+++ res/script/quiz.js
@@ -23,6 +23,8 @@ choose.textContent = 'start';
 choose.style.color = 'green';
 choose.style.backgroundColor = 'black';
 choose.style.fontFamily = 'monospace';
+choose.style.width = '50px';
+choose.style.height = '25px';
 document.body.appendChild(choose);
 
 choose.onclick = () => {
@@ -40,17 +42,21 @@ choose.onclick = () => {
 	innerbox.appendChild(msg);
 
 	let textbox = document.createElement('textarea');
+	textbox.className = 'area';
 	textbox.style.border = '3px solid green';
 	textbox.style.backgroundColor = 'black';
 	textbox.style.color = 'green';
 	textbox.style.fontFamily = 'monospace';
-	textbox.className = 'area';
+	textbox.style.width = '300px';
+	textbox.style.height = '120px';
 
 	let submit_button = document.createElement('button');
 	submit_button.style.border = '3px solid green';
 	submit_button.style.backgroundColor = 'black';
 	submit_button.style.color = 'green';
 	submit_button.style.fontFamily = 'monospace';
+	submit_button.style.width = '50px';
+	submit_button.style.height = '25px';
 	submit_button.textContent = 'submit';
 
 	innerbox.appendChild(textbox);
